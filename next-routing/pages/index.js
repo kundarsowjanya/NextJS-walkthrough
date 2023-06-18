dbb5d830d3e0462c8d9a5add318f9a3b91cb2d38
I565171 <g.sowjanya@sap.com>
2023-06-18 18:43:01 +0530

    link component navigation

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,5 +1,15 @@
-function Home(){
-    return<h1>Home Page</h1>
+import Link from "next/link"
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link href="/blog">Blog</Link>
+            <Link href='/product'>
+                Products
+            </Link>
+        </div>
+    )
 }
 
 export default Home
--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,11 +1,33 @@
-function ProductList(){
-    return(
-        <>
-         <h2>Product 1</h2>
-         <h2>Product 2</h2>
-         <h2>Product 3</h2>
-        </>
-    )
+import Link from 'next/link'
+
+function ProductList({ productId = 100 }) {
+  return (
+    <>
+      <Link href='/'>
+        Home
+      </Link>
+      <h2>
+        <Link href='product/1'>
+          Product 1
+        </Link>
+      </h2>
+      <h2>
+        <Link href='product/2'>
+          Product 2
+        </Link>
+      </h2>
+      <h2>
+        <Link href='product/3' replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`product/${productId}`}>
+          Product {productId}
+        </Link>
+      </h2>
+    </>
+  )
 }
 
-export default  ProductList
+export default ProductList
