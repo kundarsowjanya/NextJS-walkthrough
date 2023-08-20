711060575f82357f2fbb32b4457ee6ec1472ee4c
I565171 <g.sowjanya@sap.com>
2023-08-20 12:31:57 +0530

    pre-redering

---
+++ next-pre-rendering/pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+  return<h1>Next JS Pre redring</h1>
+}
+
+export default Home
---
+++ next-pre-rendering/pages/users.js
@@ -0,0 +1,32 @@
+function UserList({users}){
+    return(
+        <>
+          <h1>List of user</h1>
+          {
+              users.map((user)=>{
+                  return(
+                      <div key={user.id}>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                      </div>
+                  )
+              })
+          }
+        </>
+      
+    )
+}
+
+export default UserList
+
+export async function getStaticProps(){
+    let props = {};
+     const response= await fetch('https://jsonplaceholder.typicode.com/users');
+     const data= await response.json()
+     console.log(data)
+     return {
+        props:{
+            users:data
+        }
+      };
+}
