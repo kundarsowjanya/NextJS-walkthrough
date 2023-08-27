069eaf858ed8d124a72762749674b6d23c4add3f
I565171 <g.sowjanya@sap.com>
2023-08-27 16:54:51 +0530

    SSG with getStatucPaths

--- next-pre-rendering/pages/index.js
+++ next-pre-rendering/pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link"
+
 function Home(){
-  return<h1>Next JS Pre redring</h1>
+  return(
+  <>
+  <h1>Next JS Pre redring</h1>
+  <Link href="/users"><div>Users</div></Link>
+  <Link href="/posts"><div>Posts</div></Link>
+  </>
+  )
 }
 
 export default Home
