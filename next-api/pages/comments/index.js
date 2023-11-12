b69f3606a7aedba3e5d454c1ac67b6fbd8bb96cc
I565171 <g.sowjanya@sap.com>
2023-11-12 17:07:48 +0530

    API POST REQUEST

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -1,21 +1,43 @@
-import {useState} from 'react'
+import { headers } from '@/next.config'
+import { useState } from 'react'
 
 function CommentsPage() {
-    const [comments,setComments]=useState([])
-     
+    const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
+
     const fetchComments = async () => {
-       const response= await fetch("/api/comments")
-       const data=await response.json()
-       setComments(data)
+        const response = await fetch("/api/comments")
+        const data = await response.json()
+        setComments(data)
+    }
+    const submitButton = async () => {
+        const response = await fetch("/api/comments", {
+            method: "POST",
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
     }
     return (
         <div>
+           <input
+          type='text'
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+            <button onClick={submitButton}>
+                Submit
+            </button>
             <button onClick={fetchComments}>Load Comments</button>
+
             {
-                comments.map((comment)=>{
-                    return(
+                comments.map((comment) => {
+                    return (
                         <div id={comment.id}>
-                          {comment.id} {comment.text}
+                            {comment.id} {comment.text}
                         </div>
                     )
                 })
