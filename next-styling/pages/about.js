3cbbdca56f246f6040e29d45ae937fee32e4d990
I565171 <g.sowjanya@sap.com>
2023-11-14 19:16:42 +0530

    global style

---
+++ next-styling/pages/about.js
@@ -0,0 +1,11 @@
+function About(){
+    return(
+        <>
+        <h2>About Page</h2>
+        <button className="btn btn-success">Primary</button>
+        </>
+    )
+    
+}
+
+export default About
