534f7d705ab967af2cefffda1bba8ae41f57870c
I565171 <g.sowjanya@sap.com>
2023-11-15 17:46:18 +0530

    App Layout

---
+++ next-misc/pages/_app.js
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import "../styles/layout.css"
+export default function App({ Component, pageProps }) {
+
+  if(Component.getLayout){
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+  return(
+  <>
+   <Header/>
+   <Component {...pageProps} />
+   <Footer/>
+   </>
+  )
+}
